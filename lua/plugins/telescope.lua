6a2de37f48aa6419e888d9f27717b44eaa4e452d
Liutauras Adomaitis <liutauras.adomaitis@ibm.com>
2024-08-23 11:09:03 +0300

    Adding harpoon plugin

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -9,15 +9,22 @@ return {
         config = function()
             require("telescope").setup({
                 extensions = {
-                      ["ui-select"] = {
+                    ["ui-select"] = {
                         require("telescope.themes").get_dropdown({}),
-                      },
+                    },
+                },
+                pickers = {
+                    buffers = {
+                        sort_lastused = true,
+                        sort_mru = true
+                    }
                 },
             })
             local builtin = require("telescope.builtin")
             vim.keymap.set("n", "<C-p>", builtin.find_files, {})
             vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
             vim.keymap.set("n", "<leader><leader>", builtin.oldfiles, {})
+            vim.keymap.set("n", "<C-b>", builtin.buffers, {})
             require("telescope").load_extension("ui-select")
         end,
     },
