c6d1c3e0763364da36cb88fc8fb8b987f38fefd6
Liutauras Adomaitis <liutauras.adomaitis@adform.com>
2024-11-29 10:48:21 +0200

    gitsigns and lualine config

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -4,9 +4,26 @@ return {
         'nvim-tree/nvim-web-devicons'
     },
     config = function ()
+        local function diff_source()
+            local gitsigns = vim.b.gitsigns_status_dict
+            if gitsigns then
+              return {
+                added = gitsigns.added,
+                modified = gitsigns.changed,
+                removed = gitsigns.removed
+              }
+            end
+        end
+
         require('lualine').setup({
             options = {
                 theme = 'dracula'
+            },
+            sections = {
+                lualine_b = {
+                    {'b:gitsigns_head', icon = ''},
+                    {'diff', source = diff_source},
+                },
             }
         })
     end
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -5,13 +5,40 @@ return {
     {
         "nvim-telescope/telescope.nvim",
         tag = "0.1.8",
-        dependencies = { "nvim-lua/plenary.nvim" },
+        dependencies = {
+            { 
+                "nvim-telescope/telescope-live-grep-args.nvim" ,
+                -- This will not install any breaking changes.
+                -- For major updates, this must be adjusted manually.
+                version = "^1.0.0",
+            },
+            "nvim-lua/plenary.nvim"
+        },
         config = function()
-            require("telescope").setup({
+            local telescope = require("telescope")
+            local lga_actions = require("telescope-live-grep-args.actions")
+
+            telescope.setup({
                 extensions = {
                     ["ui-select"] = {
                         require("telescope.themes").get_dropdown({}),
                     },
+                    live_grep_args = {
+                        auto_quoting = true, -- enable/disable auto-quoting
+                        -- define mappings, e.g.
+                        mappings = { -- extend mappings
+                          i = {
+                            ["<C-s>"] = lga_actions.quote_prompt(),
+                            ["<C-i>"] = lga_actions.quote_prompt({ postfix = " --iglob " }),
+                            -- freeze the current list and start a fuzzy search in the frozen list
+                            ["<C-space>"] = lga_actions.to_fuzzy_refine,
+                          },
+                        },
+                        -- ... also accepts theme settings, for example:
+                        -- theme = "dropdown", -- use dropdown theme
+                        -- theme = { }, -- use own theme spec
+                        -- layout_config = { mirror=true }, -- mirror preview pane
+                    }
                 },
                 pickers = {
                     buffers = {
@@ -23,10 +50,12 @@ return {
             local builtin = require("telescope.builtin")
             vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find Files" })
             vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+            vim.keymap.set('n', '<leader>fs', require("telescope").extensions.live_grep_args.live_grep_args, { noremap = true })
             vim.keymap.set("n", "<leader>gs", builtin.grep_string, {})
             vim.keymap.set("n", "<leader><leader>", builtin.oldfiles, {})
             vim.keymap.set("n", "<C-b>", builtin.buffers, {})
-            require("telescope").load_extension("ui-select")
+            telescope.load_extension("ui-select")
+            telescope.load_extension("live_grep_args")
         end,
     },
 }
