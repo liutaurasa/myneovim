f32b6c84ddddbc5370b651ad017f8fb0598523f5
Liutauras Adomaitis <liutauras.adomaitis@adform.com>
2024-12-12 16:57:12 +0200

    Indent blank line configs

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -1,3 +1,4 @@
+-- Ident-blankline pluging
 return {
     {
         "lukas-reineke/indent-blankline.nvim",
@@ -5,5 +6,17 @@ return {
         -- @module "ibl"
         -- @type ibl.config
         opts = {},
+        config = function()
+            -- calling `setup` is optional for customization
+            local highlight = {"CursorColumn", "Whitespace"}
+            require("ibl").setup {
+                indent = { highlight = highlight, char = "" },
+                whitespace = {
+                    highlight = highlight,
+                    remove_blankline_trail = false,
+                },
+                scope = { enabled = false },
+            }
+        end
     }
 }
