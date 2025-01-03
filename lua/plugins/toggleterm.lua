f90d6f55479cdc59e61df42fcda59818fdcb3d5d
Liutauras Adomaitis <liutauras.adomaitis@adform.com>
2025-01-03 14:50:17 +0200

    Toogle terminal config

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -9,6 +9,15 @@ return {
                 open_mapping = [[<c-\>]],
                 hide_numbers = true,
                 auto_scroll = true,
+                direction = 'vertical', -- | 'horizontal' | 'tab' | 'float',
+                  -- size can be a number or function which is passed the current terminal
+                size = function(term)
+                  if term.direction == "horizontal" then
+                    return 15
+                  elseif term.direction == "vertical" then
+                    return vim.o.columns * 0.4
+                  end
+                end,
             })
         end,
     }
