4b4b8e58c1131aae7e53175125435ecf35182433
Liutauras Adomaitis <liutauras.adomaitis@adform.com>
2025-02-11 11:53:25 +0200

    Move cmdline to lower part of screen

---
+++ lua/plugins/noice.lua
@@ -0,0 +1,37 @@
+-- lazy.nvim
+return {
+  "folke/noice.nvim",
+  opts = {
+    -- add any options here
+    views = {
+      cmdline_popup = {
+        position = {
+          row = "70%",
+          col = "50%",
+        },
+        size = {
+          width = 60,
+          height = "auto",
+        },
+      },
+      -- popupmenu = {
+      --   relative = "editor",
+      --   position = {
+      --     row = 8,
+      --     col = "50%",
+      --   },
+      --   size = {
+      --     width = 60,
+      --     height = 10,
+      --   },
+      --   border = {
+      --     style = "rounded",
+      --     padding = { 0, 1 },
+      --   },
+      --   win_options = {
+      --     winhighlight = { Normal = "Normal", FloatBorder = "DiagnosticInfo" },
+      --   },
+      -- },
+    },
+  },
+}
