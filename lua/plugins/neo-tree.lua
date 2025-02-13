f08dc2f2bdda95e459fc577771bdd413a4709ecf
Liutauras Adomaitis <liutauras.adomaitis@adform.com>
2025-02-13 14:36:35 +0200

    Neo-tree

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,20 +1,20 @@
 return {
 	"nvim-neo-tree/neo-tree.nvim",
 	branch = "v3.x",
-    enabled = true,
+	enabled = true,
 	dependencies = {
 		"nvim-lua/plenary.nvim",
 		"nvim-tree/nvim-web-devicons",
 		"MunifTanjim/nui.nvim",
 	},
 	config = function()
-        require("neo-tree").setup({
-            close_if_last_window = true,
-            symlink_target = true
-        })
+		require("neo-tree").setup({
+			close_if_last_window = true,
+			symlink_target = true,
+		})
 		-- vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<CR>", {})
-        vim.api.nvim_set_keymap('n', '<C-n>', ':Neotree toggle=true<CR>', {noremap = true, silent = true})
+		vim.api.nvim_set_keymap("n", "<C-n>", ":Neotree toggle=true<CR>", { noremap = true, silent = true })
 		vim.keymap.set("n", "<leader>bf", ":Neotree buffers reveal float<CR>", {})
-        --
+		--
 	end,
 }
