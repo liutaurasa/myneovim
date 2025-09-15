d33e1e17d0978f5515339c798afed56f25d0f5ba
Liutauras Adomaitis <liutauras.adomaitis@adform.com>
2025-09-15 14:08:30 +0300

    C-M for snacks explorer resize

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,14 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- DO NOT USE `LazyVim.safe_keymap_set` IN YOUR OWN CONFIG!!
+-- use `vim.keymap.set` instead
+local map = vim.keymap.set
+
+-- Resize window using <ctrl> arrow keys
+map("n", "<C-M-Up>", "<cmd>resize +2<cr>", { desc = "Increase Window Height" })
+map("n", "<C-M-Down>", "<cmd>resize -2<cr>", { desc = "Decrease Window Height" })
+map("n", "<C-M-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease Window Width" })
+map("n", "<C-M-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase Window Width" })
+
