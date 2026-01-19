42fae0a7e7a4d806dd4a1a01e9b503db336926aa
Liutauras Adomaitis <liutauras.adomaitis@adform.com>
2026-01-19 10:53:26 +0200

    vim autoformat

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,7 +2,7 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 -- Set to false to disable auto format
-vim.g.autoformat = true
+vim.g.autoformat = false
 
 local options = {
     backup = false,                          -- creates a backup file
